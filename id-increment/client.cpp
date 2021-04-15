53349b136f3ce967e801e28aa7e12bf87be7643c
lwxwx <814237659@qq.com>
2021-04-15 15:04:55 +0800

    update id_increment_v1

--- id-increment/client.cpp
+++ id-increment/client.cpp
@@ -10,14 +10,14 @@
 #include <gflags/gflags.h>
 #include <butil/time.h>
 #include <brpc/channel.h>
-#include "id.pb.h"
+#include "./build/id.pb.h"
 #include <iostream>
 #include <ctime>
 using namespace std;
 
 DEFINE_string(protocol, "baidu_std", "Protocol type. Defined in src/brpc/options.proto");
 DEFINE_string(connection_type, "", "Connection type. Available values: single, pooled, short");
-DEFINE_string(server, "10.11.6.116:60006", "IP Address of server");
+DEFINE_string(server, "10.11.6.121:60006", "IP Address of server");
 DEFINE_string(load_balancer, "", "The algorithm for load balancing");
 DEFINE_int32(timeout_ms, 900000000, "RPC timeout in milliseconds");
 DEFINE_int32(max_retry, 5, "Max retries(not including the first RPC)"); 
@@ -33,7 +33,32 @@ void HandleIDcreResponse(brpc::Controller* cntl,IDIncrement::IDResponse* respons
         cout << "Fail to send EchoRequest, " << cntl->ErrorText()<<endl;
         return;
     }
-    cout<<"Received id : "<<response->message()<<endl;
+    cout << "----------------------" << endl;
+    cout<<"id = "<<response->g_id()<< " " << response->s_id() << " " <<response->m_id() << endl;
+
+    cout << "own part version = " << response->own_part_message().part_version() << " , own part address : ";
+    for(int i = 0; i < response->own_part_message().part_address().size(); i++)
+        cout << response->own_part_message().part_address(i) << " , ";
+    cout << endl;
+
+    cout << "either part version : " <<endl;
+    for(auto it = response->either_part_message().begin(); it != response->either_part_message().end(); it++){
+        cout << "part id = " << it->first << " , part version = "<< it->second.part_version() << " , either part address = " ;
+        for(auto s : it->second.part_address())
+            cout << s << " , ";
+        cout << endl;
+    }
+    
+    // for(int i = 0; i < response->partition_message().part_address().size(); i++)
+    //     cout << response->partition_message().part_address(i) << " , ";
+    // cout << endl;
+    cout << "partition lsn message :";
+    for(auto it = response->partition_alloc_lsn().begin(); it != response->partition_alloc_lsn().end(); it++){
+        cout << "id = " << it->first << ", lsns = ";
+        for(auto s : it->second.lsn())
+            cout << s << ", ";
+    }
+    cout << endl;
     // brpc::AskToQuit();
 }
 
@@ -44,11 +69,13 @@ int main(int argc, char* argv[]) {
     google::ParseCommandLineFlags(&argc, &argv, true);
 
     //log
-    logging::LoggingSettings log_setting;
-    log_setting.logging_dest=logging::LOG_TO_FILE;
-    string log_path="./id_client.log";
-    log_setting.log_file=log_path.c_str();
-    logging::InitLogging(log_setting);
+    // logging::LoggingSettings log_setting;
+    // log_setting.logging_dest=logging::LOG_TO_FILE;
+    // string log_path="./id_client.log";
+    // log_setting.log_file=log_path.c_str();
+    // logging::InitLogging(log_setting);
+
+    freopen("id_client.log","w",stdout);
     // A Channel represents a communication line to a Server. Notice that Channel is thread-safe and can be shared by all threads in your program.
     brpc::Channel channel;
 
@@ -67,7 +94,7 @@ int main(int argc, char* argv[]) {
     IDIncrement::IDService_Stub stub(&channel);
 
     // Send a request 
-    int log_id = 0;
+    // int log_id = 0;
     // while (!brpc::IsAskedToQuit()) {
     for(int i=0;i<1000;i++){
         // We will receive response synchronously, safe to put variables on stack.
@@ -77,8 +104,9 @@ int main(int argc, char* argv[]) {
         brpc::Controller* cntl = new brpc::Controller();
         // brpc::Controller cntl;
 
-        request.set_message("request id");
-        cntl->set_log_id(log_id ++);  // set by user
+        request.set_page_table_no("request id");
+        request.set_brpc_address("10.11.6.121:22595");
+        // cntl->set_log_id(log_id ++);  // set by user
 
         //callback
         google::protobuf::Closure* done = brpc::NewCallback(&HandleIDcreResponse,cntl,response);
--- id-increment/server.cpp
+++ id-increment/server.cpp
@@ -11,11 +11,12 @@
 #include <gflags/gflags.h>
 #include <brpc/server.h>
 #include <butil/logging.h>
-#include "id.pb.h"
+#include "./build/id.pb.h"
 #include <iostream>
 #include <string>
 #include <ctime>
 #include <map>
+#include <set>
 #include <mutex>
 
 using namespace std;
@@ -23,58 +24,106 @@ using namespace std;
 DEFINE_int32(port, 60006, "TCP Port of this server");
 DEFINE_int32(idle_timeout_s, -1, "Connection will be closed if there is no read/write operations during the last `idle_timeout_s'");
 
-// DEFINE_int32(socket_recv_buffer_size, 1024, 
-//             "Set the recv buffer size of socket if this value is positive");
-// DEFINE_int32(socket_send_buffer_size, 1024, 
-//             "Set send buffer size of sockets if this value is positive");
-
-long long id=1;
-map<long long,string> idmap;
+long long g_id = 0, s_id = 0, m_id = 0; // global LSN / sinple LSN / multi LSN
+map < long long, string > idmap;
 mutex id_lock;
-// int num=0;
-// // implementation of IDIncre::IDService
+map < int, mutex > part_lock; //partID --> lock
+map < int, pair< int , set < string > > > partition_message;//partID + part_version + addressesIP
+map < int, set <long long > > total_partition_lsn_message;//partID + global_LSNs
+map < int, set <long long > > send_partition_lsn_message;//partID + global_LSNs (latest message)
+map < string, int > address_part;//address belong to which partition 
+
+
 class IDIncreImpl : public IDIncrement::IDService {
 public:
     IDIncreImpl() {};
     virtual ~IDIncreImpl() {};
     virtual void IDInc(google::protobuf::RpcController* cntl_base, const IDIncrement::IDRequest* request, IDIncrement::IDResponse* response, google::protobuf::Closure* done) {
-        // This object helps you to call done->Run() in RAII style. If you need to process the request asynchronously, pass done_guard.release().
         brpc::ClosureGuard done_guard(done);
 
         brpc::Controller* cntl = static_cast<brpc::Controller*>(cntl_base);
 
-		//lock id
+        IDIncrement::PartMessage* partMess = new IDIncrement::PartMessage;//send partition_message
+        // std::stringstream ip_address;
+        // ip_address << cntl->remote_side().ip;
+
+        IDIncrement::LogMessage* logMess = new IDIncrement::LogMessage;
+
         lock_guard<mutex> guard(id_lock);
-        // num++;
-        // sleep(1);
-        //time
-       // time_t now = time(0);
-       // char* dt = ctime(&now);
+        g_id++;
+        response->set_g_id(g_id);
+        // lock_guard< mutex > guard(part_lock[address_part[ip_address.str()]]);
+        // int part_id = address_part[ip_address.str()];
+        int part_id = address_part[request->brpc_address()];
+        int s_id = total_partition_lsn_message[part_id].size() + 1;
+        int m_id = 0;
+        //TODO: s_id 和 m_id要不要size+1 取决于mysql里面找空洞是从 1 还是 0 开始找的
         
-        // idmap[butil::endpoint2str(cntl->remote_side()).c_str()]=id;
-        cout<<"Received request[log_id="<<cntl->log_id()<<"] from " << cntl->remote_side() << " to " << cntl->local_side()<< ": " << request->message()<<" ,allot id: "<<id<< endl;//" ,time:"<<dt<<endl;
-        // Fill response.
-        response->set_message(to_string(id));
-        idmap[id]=butil::endpoint2str(cntl->remote_side()).c_str();
-        id++;
+        cout<<"Received request from " << cntl->remote_side() << " to " << cntl->local_side()<< ": " << request->page_table_no()<<" ,allot id: "<<g_id<< endl;//" ,time:"<<dt<<endl;
+        cout << "part_id = " << part_id << endl;
+        // response->set_allocated_partition_message(partMess);
+        total_partition_lsn_message[part_id].insert(g_id);
+        send_partition_lsn_message[part_id].insert(g_id);
+
+        if(part_id == 0){
+            //P0不用知道m-id，他是通过s-id来实现排序的
+            // m_id = total_partition_lsn_message[0].size();
+
+            //只用告诉P0其他分区的lsn分配情况
+            auto it = send_partition_lsn_message.begin();
+            for(it++; it != send_partition_lsn_message.end(); it++){
+                // logMess.add_lsn(s);
+                auto *m = response->mutable_partition_alloc_lsn();
+                for(auto s : it->second)
+                    (*m)[it->first].add_lsn(s);
+                send_partition_lsn_message[it->first].clear();
+            }
+
+            //告诉P0其他分区的地址情况
+            auto *p = response->mutable_either_part_message();
+            auto it1 = partition_message.begin();
+            for(it1++; it1 != partition_message.end(); it1++){
+                // partMess->set_part_version(it->second.first);
+                (*p)[it1->first].set_part_version(it1->second.first);
+                for(auto s : it1->second.second)
+                    (*p)[it1->first].add_part_address(s);
+            }
+        }
+        else{
+            //Pi的m-id应该和P0的s-id一致，就是表示要等到几号id来
+            m_id = total_partition_lsn_message[0].size();
+            //告诉Pi：P0分区的地址情况
+            auto *p = response->mutable_either_part_message();
+            (*p)[0].set_part_version(partition_message[0].first);
+            for(auto s : partition_message[0].second)
+                (*p)[0].add_part_address(s);
+        }
+
+        response->set_s_id(s_id);
+        response->set_m_id(m_id);
+        //告诉自己分区的地址情况
+        partMess->set_part_version(partition_message[part_id].first);//TODO: 分区有改变的时候这个版本号也应该会改变，这个后期再改，现在版本号不改变就都是1
+        for(auto it : partition_message[part_id].second)
+            partMess->add_part_address(it); 
+        response->set_allocated_own_part_message(partMess);
     }
 };
 
 int main(int argc, char* argv[]) {
-    // Parse gflags. We recommend you to use gflags as well.
+
+    //TODO: 先假设没有动态分区，而是在调度器这边就划分好了分区；动态分区后期用并查集在server这边改一下就好，client应该是不用大改的
+    //这边现在没有设端口了，就是一台服务器上的一定是一个分区||这里是需要设置端口的，这是BRPC交流的端口啊
+    partition_message[1].first = 1;
+    partition_message[1].second.insert("10.11.6.117:22595");
+    partition_message[0] .first = 1;
+    partition_message[0].second.insert("10.11.6.121:22595");
+    address_part["10.11.6.117:22595"] = 1;
+    address_part["10.11.6.121:22595"] = 0;
+
     google::ParseCommandLineFlags(&argc, &argv, true);
 
-    // Generally you only need one Server.
     brpc::Server server;
 
-    //log
-    // logging::LoggingSettings log_setting;
-    // log_setting.logging_dest=logging::LOG_TO_FILE;
-    // string log_path="./id_server.log";
-    // log_setting.log_file=log_path.c_str();
-    // logging::InitLogging(log_setting);
-
-    // Instance of your service.
     IDIncreImpl id_service_impl;
 
     // Add the service into server. 
@@ -86,22 +135,15 @@ int main(int argc, char* argv[]) {
     // Start the server.
     brpc::ServerOptions options;
     options.idle_timeout_sec = FLAGS_idle_timeout_s;
-//	options.socket_recv_buffer_size = 512;
-//	options.socket_send_buffer_size = 512;
+    
     if (server.Start(FLAGS_port, &options) != 0) {
         cout << "Fail to start Server"<<endl;
         return -1;
     }
     cout<<"Server start"<<endl;
 
-    // Wait until Ctrl-C is pressed, then Stop() and Join() the server.
+
     server.RunUntilAskedToQuit();
-    // cout<<num<<endl;
-    // freopen("map.out","w",stdout);
-    // map<long long,string>::iterator it;
-    // for(it=idmap.begin();it!=idmap.end();it++)
-    // {
-    //     cout<<"idmap["<<it->first<<"]="<<it->second<<endl;
-    // }
+    
     return 0;
 }
