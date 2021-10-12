0fac39398e436712a5df6ba4350c3b61e9b51b79
wxwxlwx <814237659@qq.com>
2021-10-12 10:20:54 +0800

    2021-10-12 : correct script for origin Percona-TransactionLog

---
+++ id-increment/origin-id-increment/build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liuwenxin/id-increment/client.cpp" "/home/liuwenxin/id-increment/build/CMakeFiles/client.dir/client.cpp.o"
+  "/home/liuwenxin/id-increment/id.pb.cc" "/home/liuwenxin/id-increment/build/CMakeFiles/client.dir/id.pb.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/liuwenxin/Percona/brpc_env/openssl_1_0_2k-installa/include"
+  "/home/liuwenxin/Percona/brpc_env/output/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
